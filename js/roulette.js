d3012f888bf0b97181169d8ec2b62edd51c34823
SKYEND <theskyend0@gmail.com>
2018-06-25 09:03:35 -0700

    canvas

--- js/roulette.js
+++ js/roulette.js
@@ -1,7 +1,11 @@
 (function($){
     $.fn.skyRoulette = function(options){
         var target = this;
+        var partitions = options.partition;
+        var rps = options.rotationPerSec || 5;
 
-        console.log(target);
+
+
+
     }
 })(jQuery);
